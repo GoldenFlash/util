a63e468431f22c1e44e9bd34075662371ac0b34b
slidewater <892458829@qq.com>
2018-09-13 22:34:47 +0800

    1

--- util.js
+++ util.js
@@ -1,3 +1,25 @@
 function isUndef (v) {
   return v === undefined || v === null
 }
+function isDef (v) {
+  return v !== undefined && v !== null
+}
+function isTrue (v) {
+  return v === true
+}
+
+function isFalse (v) {
+  return v === false
+}
+function isPrimitive (value) {
+  return (
+    typeof value === 'string' ||
+    typeof value === 'number' ||
+    // $flow-disable-line
+    typeof value === 'symbol' ||
+    typeof value === 'boolean'
+  )
+}
+function isObject (obj) {
+  return obj !== null && typeof obj === 'object'
+}

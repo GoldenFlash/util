ca55c20ba8a710ba2cee2f319b1e8043f034649e
GoldenFlash <892458829@qq.com>
2019-04-25 10:42:26 +0800

    .

--- util.js
+++ util.js
@@ -79,3 +79,17 @@ function IsDate(sm, mystring) {
   }
   return true;
 }
+//在XMLHttprequest中对象转表单编码
+function encodeFormData(data){
+  if(!data){return ""}
+  var pairs = []
+  for(var name in data){
+    if(!data.hasOwnProperty(name)) continue
+    if(typeof data[name] === "function" ) continue
+    var value = data[name].toString()
+    name = encodeURIComponent(name.replace("%20","+"))
+    value = encodeURIComponent(value.replace("%20","+"))
+    pairs.push(name + "=" + value)
+  }
+  return pairs.join("&")
+}
